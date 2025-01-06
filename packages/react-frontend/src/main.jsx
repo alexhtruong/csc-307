e9170b93edccdbbea2351856fed1fae5ae983060
Alex Truong <46734496+alexhtruong@users.noreply.github.com>
2025-01-06 11:08:35 -0800

    full table functionality added

---
+++ packages/react-frontend/src/main.jsx
@@ -0,0 +1,15 @@
+// src/main.jsx
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+import MyApp from "./App";
+
+
+// Create the container
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the Root
+root.render(<MyApp />);
